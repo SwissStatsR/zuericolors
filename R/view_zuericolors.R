5fb278161a698ce2e6ebf890f97d6512b5f03c6f
alfell <alessandro.feller@zuerich.ch>
2022-06-01 19:12:33 +0200

    Function Update

--- R/view_zuericolors.R
+++ R/view_zuericolors.R
@@ -9,6 +9,7 @@
 #'
 #' @import scales
 #' @import ggplot2
+#' @import ggpubr
 #'
 #' @examples
 #' # View colors of palette "harmonic6"
@@ -16,9 +17,29 @@
 view_zuericolors <- function(palette = " ") {
   choosenPalette <- deparse(substitute(palette))
   if (missing(palette))  {
-    warning <- c('You have not defined a palette. Please choose one of the following:\n')
-    availablePalettes <- names(palettes)
-    return(cat(warning, availablePalettes, sep = "\n"))
+    plotList <- lapply(names(palettes),
+                       function(paletteName) {
+                         name <- names(palettes[paletteName])
+                         color <- palettes[[paletteName]]
+                         value <- seq(1:length(color))
+                         data <- data.frame(value, color)
+                         p <- ggplot(data = data,
+                                     aes(x = as.factor(value),
+                                         fill = as.factor(value),
+                                         label = color)) +
+                           scale_fill_manual(values = c(color)) +
+                           scale_y_continuous(limits = c(-1,1)) +
+                           geom_bar() +
+                           theme_void() +
+                           labs(title = name) +
+                           theme(legend.position = "none",
+                                 axis.text.x = element_blank(),
+                                 plot.title = element_text(size = 10,
+                                                           hjust = 0.5))
+                         p
+                       })
+    allplots <- ggarrange(plotlist = plotList)
+    return(allplots)
   }
   if (is.null(palettes[[palette]])) {
     warning <- paste0('The palette ', choosenPalette, ' does not exist. Please choose one of the following:\n')
@@ -42,7 +63,9 @@ view_zuericolors <- function(palette = " ") {
       theme_void() +
       labs(title = name) +
       theme(legend.position = "none",
-            axis.text.x = element_blank())
+            axis.text.x = element_blank(),
+            plot.title = element_text(size = 20,
+                                      hjust = 0.5))
     return(p)
   }
 }
