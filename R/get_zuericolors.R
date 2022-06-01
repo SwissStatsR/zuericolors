dbf11e58892ed2328a539bed00e1055a0997b483
alfell <alessandro.feller@zuerich.ch>
2022-06-01 15:12:28 +0200

    Documentation

---
+++ R/get_zuericolors.R
@@ -0,0 +1,42 @@
+#' Zuericolors Palettes
+#'
+#' @description Function to access color values of a specific zuericolors palette.
+#'
+#' @param palette Character that names the color palette
+#'
+#' @param nth Choose specific color of a specific palette
+#'
+#' @examples
+#' # Get colors of palette "harmonic6"
+#' get_zuericolors("harmonic6")
+#'
+#' # Get third color value of palette "harmonic6"
+#' get_zuericolors("harmonic6", 3)
+#'
+#' # Get first three color values of palette "harmonic6"
+#' get_zuericolors("harmonic6", 1:3)
+#'
+#' @export
+get_zuericolors <- function(palette, nth) {
+  choosenPalette <- deparse(substitute(palette))
+  if (missing(palette) & missing(nth)) {
+    lists <- lapply(palettes, head)
+    return(lists)
+  }
+  if (missing(palette) & !missing(nth)) {
+    warning <- c('You have not defined a palette. Please choose one of the following:\n')
+    availablePalettes <- names(palettes)
+    return(cat(warning, availablePalettes, sep = "\n"))
+  } else {
+    palette <- palettes[[palette]]
+    color <- palette[nth]
+    if (is.null(color)) {
+      warning <- paste0('The palette ', choosenPalette, ' does not exist. Please choose one of the following:\n')
+      availablePalettes <- names(palettes)
+      return(cat(warning, availablePalettes, sep = "\n"))
+    } 
+    else {
+      return (color)
+    }
+  }
+}
---
+++ R/view_zuericolors.R
@@ -0,0 +1,48 @@
+#' View Zuericolors Palettes
+#'
+#' @description Function to visually inspect values of a specific zuericolors palette
+#'
+#' @param palette character vector
+#'
+#' @return
+#' @export
+#'
+#' @import scales
+#' @import ggplot2
+#'
+#' @examples
+#' # View colors of palette "harmonic6"
+#' view_zuericolors("harmonic6")
+view_zuericolors <- function(palette = " ") {
+  choosenPalette <- deparse(substitute(palette))
+  if (missing(palette))  {
+    warning <- c('You have not defined a palette. Please choose one of the following:\n')
+    availablePalettes <- names(palettes)
+    return(cat(warning, availablePalettes, sep = "\n"))
+  }
+  if (is.null(palettes[[palette]])) {
+    warning <- paste0('The palette ', choosenPalette, ' does not exist. Please choose one of the following:\n')
+    availablePalettes <- names(palettes)
+    return(cat(warning, availablePalettes, sep = "\n"))
+  } else {
+    name <- names(palettes[palette])
+    color <- palettes[[palette]]
+    value <- seq(1:length(color))
+    data <- data.frame(value, color)
+    p <- ggplot(data = data,
+                aes(x = as.factor(value),
+                    fill = as.factor(value),
+                    label = color)) +
+      scale_fill_manual(values = c(color)) +
+      scale_y_continuous(limits = c(-1,1)) +
+      geom_bar() +
+      geom_text(y = -0.05,
+                angle = 90,
+                hjust = 1) +
+      theme_void() +
+      labs(title = name) +
+      theme(legend.position = "none",
+            axis.text.x = element_blank())
+    return(p)
+  }
+}
