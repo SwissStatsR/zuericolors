5a881ed5c8a4b0347d296d145317970943bb2543
alfell <alessandro.feller@zuerich.ch>
2022-12-21 14:08:19 +0100

    Definitive Palettes

--- R/palettes.R
+++ R/palettes.R
@@ -5,17 +5,15 @@
 palettes <- list(
   "seq6gry" = c("#FAFAFA", "#EAEAEA", "#D6D6D6", "#B8B8B8", "#7C7C7C", "#545454"),
   "qual12br" = c("#5D4BFE", "#4AA9FF", "#55FFFF", "#986AD5", "#FC4C99", "#FF919A", "#349894", "#44B14A", "#B7E14E", "#B97624", "#FF7231", "#FFD736"),
-  "qual12" = c("#3431DE", "#0A8DF6", "#23C3F1", "#7B4FB7", "#DB247D", "#FB737E", "#007C78", "#1D942E", "#99C32E", "#9A5B01", "#FF720C", "#FBB900"),
+  "qual12" = c("#3431DE", "#0A8DF6", "#23C3F1", "#7B4FB7", "#DB247D", "#FB737E", "#007C78", "#1F9E31", "#99C32E", "#9A5B01", "#FF720C", "#FBB900"),
   "qual12da" = c("#0017BF", "#0072D7", "#00A5D2", "#5E359A", "#BA0062", "#DA5563", "#00615D", "#00770F", "#7BA600", "#7B4100", "#DC5500", "#DA9C00"),
-  "qual6" = c("#3431DE", "#DB247D", "#1D942E", "#FBB900", "#23C3F1", "#FF720C"),
+  "qual6" = c("#3431DE", "#DB247D", "#1F9E31", "#FBB900", "#23C3F1", "#FF720C"),
   "qual6a" = c("#3431DE", "#0A8DF6", "#23C3F1", "#7B4FB7", "#DB247D", "#FB737E"),
-  "qual6b" = c("#007C78", "#1D942E", "#99C32E", "#9A5B01", "#FF720C", "#FBB900"),
+  "qual6b" = c("#007C78", "#1F9E31", "#99C32E", "#9A5B01", "#FF720C", "#FBB900"),
   "seq9blu" = c("#CADEFF", "#AEC2FF", "#93A6FF", "#778AFF", "#5B6EFF", "#4D59E2", "#3E44C5", "#302FA7", "#211A8A"),
   "seq9red" = c("#FED2EE", "#FEAED6", "#F589BE", "#F165A5", "#ED408D", "#D1307B", "#B52069", "#991056", "#7D0044"),
   "seq9grn" = c("#CFEED8", "#A8E0B3", "#81D18F", "#5BC36A", "#34B446", "#2A9A3C", "#208032", "#166529", "#0C4B1F"),
   "seq9brn" = c("#FCDDBB", "#F7BD8C", "#F39D5E", "#EE7D2F", "#EA5D00", "#C84E00", "#A53E00", "#832F00", "#611F00"),
-  "div9val" = c("#782600", "#CC4309", "#FF720C", "#FFBC88", "#FFFFFF", "#AECBFF", "#6B8EFF", "#3B51FF", "#2F2ABB"),
-  "div9ntr" = c("#A30059", "#DB247D", "#FF579E", "#FFA8D0", "#FFFFFF", "#A8DBB1", "#55BC5D", "#1D942E", "#10652A"),
-  "div9valgry" = c("#782600", "#CC4309", "#FF720C", "#FFBC88", "#E4E0DF", "#AECBFF", "#6B8EFF", "#3B51FF", "#2F2ABB"),
-  "div9ntrgry" = c("#A30059", "#DB247D", "#FF579E", "#FFA8D0", "#E4E0DF", "#A8DBB1", "#55BC5D", "#1D942E", "#10652A")
+  "div9val" = c("#782600", "#CC4309", "#FF720C", "#FFBC88", "#E4E0DF", "#AECBFF", "#6B8EFF", "#3B51FF", "#2F2ABB"),
+  "div9ntr" = c("#A30059", "#DB247D", "#FF579E", "#FFA8D0", "#E4E0DF", "#A8DBB1", "#55BC5D", "#1F9E31", "#10652A")
 )
